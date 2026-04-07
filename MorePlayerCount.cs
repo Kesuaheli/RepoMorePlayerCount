198aebf9688d604ac8241b993ef03d77969c1680
Kesuaheli <kesuaheligame@gmail.com>
2026-04-07 02:35:28 +0200

    feat: add logic for increasing upgrade amount
    Hardcoded it to 3 for now, since I dont know how to get the actual count of players.

--- MorePlayerCount.cs
+++ MorePlayerCount.cs
@@ -1,5 +1,27 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
 
+[BepInPlugin(modGUID, modName, modVersion)]
 public class MorePlayerCount : BaseUnityPlugin
 {
+    const string modGUID = "de.kesuaheli.moreplayercount";
+    const string modName = "MorePlayerCount";
+    const string modVersion = "0.0.1";
+    private void Awake()
+    {
+        var harmony = new Harmony(modGUID);
+        harmony.PatchAll();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        Logger.LogInfo($"Mod Loaded!");
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!SemiFunc.RunIsShop()) return;
+
+        StatsManager.instance.itemDictionary.GetValueSafe("Item Upgrade Map Player Count").maxAmount = 3;
+    }
 }
